4c0ff0074fb217538555aa8ab6eedbfb241aa47a
CDCSteki <david.cozi@yahoo.com>
2026-05-10 14:48:30 +0300

    generate Angular comp and services

---
+++ frontend/src/app/features/appointments/appointment-new/appointment-new.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-new',
+  imports: [],
+  templateUrl: './appointment-new.html',
+  styleUrl: './appointment-new.scss',
+})
+export class AppointmentNew {}
---
+++ frontend/src/app/features/dashboard/mechanic-dashboard/mechanic-dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mechanic-dashboard',
+  imports: [],
+  templateUrl: './mechanic-dashboard.html',
+  styleUrl: './mechanic-dashboard.scss',
+})
+export class MechanicDashboard {}
